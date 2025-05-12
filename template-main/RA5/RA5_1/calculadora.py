a34dc65ab2354fef4bb6e37c3ad21fdadedab82f
PPS10836126 <xavgimreu@alu.edu.gva.es>
2025-05-12 10:04:59 +0200

    Add files via upload

---
+++ template-main/RA5/RA5_1/calculadora.py
@@ -0,0 +1,11 @@
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) != 3:
+        print("Uso: python calculadora.py <num1> <num2>")
+    else:
+        resultado = multiplicar(int(sys.argv[1]), int(sys.argv[2]))
+        print(f"Resultado: {resultado}")
---
+++ template-main/RA5/RA5_1/test_calculadora.py
@@ -0,0 +1,11 @@
+import unittest
+from calculadora import multiplicar
+
+class TestCalculadora(unittest.TestCase):
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(2, 3), 6)
+        self.assertEqual(multiplicar(-1, 5), -5)
+        self.assertEqual(multiplicar(0, 100), 0)
+
+if __name__ == '__main__':
+    unittest.main()
